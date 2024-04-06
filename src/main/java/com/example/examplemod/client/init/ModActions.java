11870852fd7d9837ccb9673174d7446a4f236f9c
Jez <Jinenze0810@163.com>
2024-04-06 17:32:42 +0800

    1.20.4

--- src/main/java/com/example/examplemod/client/init/ModActions.java
+++ src/main/java/com/example/examplemod/client/init/ModActions.java
@@ -2,7 +2,6 @@ package com.example.examplemod.client.init;
 
 import com.example.examplemod.client.action.Action;
 import com.example.examplemod.client.action.Dodge;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class ModActions {
     public static void register() {
